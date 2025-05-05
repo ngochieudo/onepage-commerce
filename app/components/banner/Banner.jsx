23e08dce43cf1115c50ba8edf9b05cb10edf993c
Hieu Do <hieudngch190863@fpt.edu.vn>
2025-05-06 02:51:41 +0700

    add backgound for banner section

---
+++ app/components/banner/Banner.jsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+  return (
+    <div className="w-[100vw] h-[100vh] bg-[url('/green_env.png')] bg-cover bg-center flex items-center justify-center">
+
+    </div>
+  )
+}
+
+export default Banner
