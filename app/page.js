cac1455d084ecbcc48bfd9192e61d93e4b5487f5
Hieu Do <hieudngch190863@fpt.edu.vn>
2025-05-06 02:53:53 +0700

    form structure of the page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
+import Banner from "./components/banner/Banner";
 import Navbar from "./components/navbar/Navbar";
+import TopUp from "./components/topup/TopUp";
+import Voucher from "./components/voucher/Voucher";
 
 export default function Home() {
   return (
-    <div className="">
-      <Navbar/>
-    </div>
+    <>
+      <Navbar />
+      <Banner />
+      <Voucher />
+      <TopUp />
+    </>
   );
 }
