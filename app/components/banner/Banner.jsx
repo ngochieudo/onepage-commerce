bcdf122b56c1fe9f461f4365da8b9a5a3d13c04f
Hieu Do <hieudngch190863@fpt.edu.vn>
2025-05-06 02:56:47 +0700

    onepage commerce

--- app/components/banner/Banner.jsx
+++ app/components/banner/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <div className="w-[100vw] h-[100vh] bg-[url('/green_env.png')] bg-cover bg-center flex items-center justify-center">
+    <div className="w-[100vw] h-[100vh] bg-[url('/bg/green_env.png')] bg-cover bg-center">
 
     </div>
   )
---
+++ app/components/topup/TopUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopUp = () => {
+  return (
+    <div className="w-[100vw] h-[100vh] bg-[url('/bg/cold_env.png')] bg-cover bg-center">
+
+    </div>
+  )
+}
+
+export default TopUp
